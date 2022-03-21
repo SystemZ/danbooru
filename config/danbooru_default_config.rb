9da9bb0b2f8ebfc4e2f7239daf3b93b9bc7ccbbe
Michał Frąckiewicz <1836094+SystemZ@users.noreply.github.com>
2022-03-21 16:29:36 +0100

    Config entry for max video duration

--- config/danbooru_default_config.rb
+++ config/danbooru_default_config.rb
@@ -171,6 +171,11 @@ module Danbooru
       40000
     end
 
+    # Maximum duration of an video in seconds.
+    def max_video_duration
+      140
+    end
+    
     # How long pending posts stay in the modqueue before being deleted.
     def moderation_period
       3.days
